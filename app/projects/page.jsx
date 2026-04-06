2f07636cb079759a878d78c5a1645206824d21eb
Ishanvi Upare <ishanviupare@gmail.com>
2026-04-06 12:39:17 -0500

    background for projects

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const projects = () => {
   return (
-    <div>projects</div>
+    <div className = "h-screen bg-[#c6b0ad] flex flex-col font-serif text-[#5a4a46]">
+
+      <div className = "text-6xl p-20">
+           Projects...
+      </div>
+      <div className = "p-20">
+        Personal Website
+      </div>
+
+    </div>
   )
 }
 
